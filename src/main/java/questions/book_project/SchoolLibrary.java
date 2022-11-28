80ad11dc48d6a9f966fcaf0164bf7b6a616143f0
Mustafa kart <46137737+mustafakart000@users.noreply.github.com>
2022-11-28 18:09:25 +0300

    Update SchoolLibrary.java

--- src/main/java/questions/book_project/SchoolLibrary.java
+++ src/main/java/questions/book_project/SchoolLibrary.java
@@ -1,19 +1,15 @@
 package questions.book_project;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
 //----------------------------------------------------CLASS-------------------------------------------------------------
 public class SchoolLibrary {
-    static Integer counter=1000;
-    SchoolLibrary(){
-        SchoolLibrary.counter++;
-//      System.out.println("constructor body: "+bookList);
-    }
+    static Integer counter=1006;
+    static Integer select;
+
     static HashMap<String, String> bookList= new HashMap<>();
 
 //--------------------------------------------------MAİN METHOD---------------------------------------------------------
     public static void main(String[] args) {
-        beforeList();
         selectProcess();
 
     }
@@ -22,38 +18,51 @@ public class SchoolLibrary {
 // selectProcess() variable: select-------------------
     static void selectProcess(){
         System.out.println("1: add Book 2: remove Book 3:find the book by entering the book information " +
-                "5: show all books 6:exit");
+                "4: show all books 5:exit");
         Scanner scan = new Scanner(System.in);
-        Integer select=scan.nextInt();
+        select=scan.nextInt();
         switch (select) {
+
             case 1:
                 addBook();
                 showBookList();
+                selectProcess();
                 break;
             case 2:
                 removeBook();
                 showBookList();
+                selectProcess();
                 break;
             case 3 :
                 findBookWithBookType();
                 showBookList();
+                selectProcess();
                 break;
-        }selectProcess();
+            case 4:
+                showBookList();
+                selectProcess();
+            case 5:
+                System.out.println("Sağlıklı günler dileriz.");
+                break;
+        }
+
 
 
     }
 
 
-//--addBook()---variable:bookName, author, price, allInfoOfBook and bookList--------------
+//-- ---variable:bookName, author, price, allInfoOfBook and bookList--------------
+    //demo test için öncesinde 5 adet kitap ekledik.
     static void beforeList(){
+        System.out.println("======================================================");
+        bookList.put("Enstitü", "1005 Enstitü Stephen King 81 Korku-gerilim");
         bookList.put("Süt Lekesi", "1001 Süt Lekesi Esra Ezmeci 50.20 Bilim Kurgu");
         bookList.put("Kozmos", "1002 Süt Lekesi Carl Sagan 55 Bilim Kurgu");
         bookList.put("Beyin", "1003 Beyin David Eagleman 47 Bilim Kurgu");
-        bookList.put("Saklambaç", "1004 Saklambaç Kabal 42 Korku-gerilim");
-        bookList.put("Hizmetçi", "1005 Hizmetçi Nita Prose 59 Korku-gerilim");
-        bookList.put("Enstitü", "1006 Enstitü Stephen King 81 Korku-gerilim");
+        bookList.put("Saklambaç", "1003 Saklambaç Kabal 42 Korku-gerilim");
+        bookList.put("Hizmetçi", "1004 Hizmetçi Nita Prose 59 Korku-gerilim");
     }
-
+//HashMap ile kitap ekleme addBook() methodu.
     static HashMap<String,String> addBook(){
         Scanner scan=new Scanner(System.in);
         System.out.println("Kitap Adı: ");
@@ -63,10 +72,9 @@ public class SchoolLibrary {
         System.out.println("Kitabın fiyatı: ");
         String price= scan.next();
         System.out.println();
-        String allInfoOfBook= counter+"-"+author+"-"+price+"-"+bookType();
         SchoolLibrary schLib= new SchoolLibrary();
+        String allInfoOfBook= (SchoolLibrary.counter++)+" "+author+" "+price+" "+bookType();
         bookList.put(bookName, allInfoOfBook);
-        System.out.println(bookList);
         showBookList();
         selectProcess();
         return bookList;
@@ -80,14 +88,13 @@ public class SchoolLibrary {
         String getBook=scan.next();
         try {
             bookList.remove(getBook);
-            System.out.println(bookList);
-        }catch (ClassCastException e) {
+        }catch (InputMismatchException e) {
             showBookList();
             System.out.println("Lütfen ismin var olup olmadığını kontrol ediniz."+e.getMessage());
         }
 
         }
-
+//bookType() kategorize etme methodu.(findBookWithBookType() ve addBook() methodları için bu methodu oluşturduk)
     static String bookType(){
         Scanner scan = new Scanner(System.in);
         String getBookType="";
@@ -96,33 +103,41 @@ public class SchoolLibrary {
         String[] bookType ={"Bilim Kurgu", "Korku-gerilim","polisiye"};
         switch (userBookType){
             case 1:
-                getBookType=getBookType+bookType[0];
+                getBookType=getBookType+bookType[0]; //Bilim Kurgu
                 break;
             case 2:
-                getBookType=getBookType+bookType[1];
+                getBookType=getBookType+bookType[1]; //Korku-gerilim
                 break;
             case 3:
-                getBookType=getBookType+bookType[2];
+                getBookType=getBookType+bookType[2];//polisiye
                 break;
         }showBookList();
         return getBookType;
 
     }
+    //findBookWithBookType() kategorisine göre kitapları listele
     static void findBookWithBookType() {
         Scanner scan = new Scanner(System.in);
         System.out.println("kitap türüne göre arama yapmak için" +
                 "\"Bilim Kurgu: 1   Korku-gerilim: 2   polisiye: 3\" birini seçiniz ");
         String typeOfBook = bookType();
-        new SchoolLibrary();
-        if (typeOfBook == bookList.values().toString().split("-")[3]) {
-            for (String w:bookList.values()){
-                System.out.println(w.split("-")[3].contains(typeOfBook));
-            }
-        }else {
+        int counter=0;
+        Set<Map.Entry<String,String>> entries = bookList.entrySet();
+            for (Map.Entry<String,String> w:entries){
+                if (w.getValue().contains(typeOfBook)) {
+                    System.out.println(typeOfBook+" Kategori listesi: "+w);
+                    counter++;
+                }
+
+        }System.out.println(typeOfBook+" Kategori listesinde: "+counter+" adet var.");
+            /*else {
             System.out.println("Belirttiğiniz. katogeri mevcut değildir.");
-            selectProcess();//
-        }
+            //
+        }*/selectProcess();
     }
+
+
+//showBookList() Kitapları göster
     static void showBookList(){
         HashMap<String, HashMap> selects=new HashMap<>();
         SchoolLibrary sch=new SchoolLibrary();
@@ -132,6 +147,8 @@ public class SchoolLibrary {
                         String value= w.getValue();
             System.out.println("key: "+key+" Value: "+value);
         }
+        System.out.println("=============================++++++++++======================================");
+        beforeList();
     }
 }
 
